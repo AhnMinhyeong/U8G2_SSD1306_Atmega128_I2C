b35e80fb7167c194a9907b3f35681b6696cd7249
AhnMinhyeong <44267756+AhnMinhyeong@users.noreply.github.com>
2018-11-02 02:00:34 +0900

    Update SSD1306_Atmega128_Func.c

--- SSD1306_Atmega128_Func.c
+++ SSD1306_Atmega128_Func.c
@@ -89,6 +89,7 @@ uint8_t u8x8_byte_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_p
 
 uint8_t gpio_and_delay_atmega128(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
+	/*
 	switch(msg)
 	{
 		case U8X8_MSG_GPIO_AND_DELAY_INIT:	// called once during init phase of u8g2/u8x8
@@ -107,5 +108,6 @@ uint8_t gpio_and_delay_atmega128(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, voi
 		break;
 
 	}
+	*/
 	return 1;
 }
