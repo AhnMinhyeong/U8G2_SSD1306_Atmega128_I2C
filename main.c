2d2b7b21892c932771f8650301173c883f144836
AhnMinhyeong <44267756+AhnMinhyeong@users.noreply.github.com>
2018-11-02 00:26:54 +0900

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,51 @@
 u8g2_t u8g2;
 //u8g2_t u8g2_ks0108;
 
+void init_ssd1306(void)
+{
+	char data[25];
+	
+	data[0]=0xae;
+	data[1]=0x00;
+	data[2]=0x10;
+	data[3]=0x40;
+	data[4]=0xb0;
+	data[5]=0x81;
+	data[6]=0xff;
+	data[7]=0xa1;
+	data[8]=0xa6;
+	data[9]=0xc9;
+	data[10]=0xa8;
+	data[11]=0x3f;
+	data[12]=0xd3;
+	data[13]=0x00;
+	data[14]=0xd5;
+	data[15]=0x80;
+	data[16]=0xd9;
+	data[17]=0xf1;
+	data[18]=0xda;
+	data[19]=0x12;
+	data[20]=0xdb;
+	data[21]=0x40;
+	data[22]=0x8d;
+	data[23]=0x14;
+	data[24]=0xaf;
+	i2c_initialize();
+
+	_delay_ms(100);
+	PORTD&=~(1<<3);
+	_delay_ms(1);
+	PORTD|=(1<<3);
+
+	for(int i=0;i<25;i++)
+	{
+		i2c_Start(OLED_ADDR);
+		i2c_write(0x00);
+		i2c_write(data[i]);
+		i2c_Stop();
+	}
+}
+
 uint8_t u8x8_byte_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
 	uint8_t *data;
@@ -54,6 +99,7 @@ uint8_t gpio_and_delay_atmega128(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, voi
 	switch(msg)
 	{
 		case U8X8_MSG_GPIO_AND_DELAY_INIT:	// called once during init phase of u8g2/u8x8
+		
 		break;							// can be used to setup pins
 		case U8X8_MSG_DELAY_NANO:			// delay arg_int * 1 nano second
 		break;
@@ -69,51 +115,6 @@ uint8_t gpio_and_delay_atmega128(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, voi
 
 	}
 	return 1;
-}
-
-void init_ssd1306(void)
-{
-	char data[25];
-	
-	data[0]=0xae;
-	data[1]=0x00;
-	data[2]=0x10;
-	data[3]=0x40;
-	data[4]=0xb0;
-	data[5]=0x81;
-	data[6]=0xff;
-	data[7]=0xa1;
-	data[8]=0xa6;
-	data[9]=0xc9;
-	data[10]=0xa8;
-	data[11]=0x3f;
-	data[12]=0xd3;
-	data[13]=0x00;
-	data[14]=0xd5;
-	data[15]=0x80;
-	data[16]=0xd9;
-	data[17]=0xf1;
-	data[18]=0xda;
-	data[19]=0x12;
-	data[20]=0xdb;
-	data[21]=0x40;
-	data[22]=0x8d;
-	data[23]=0x14;
-	data[24]=0xaf;
-	i2c_initialize();
-
-	_delay_ms(100);
-	PORTD&=~(1<<3);
-	_delay_ms(1);
-	PORTD|=(1<<3);
-
-	for(int i=0;i<25;i++)
-	{
-		i2c_Start(OLED_ADDR);
-		i2c_write(0x00);
-		i2c_write(data[i]);
-		i2c_Stop();
-	}
 }
 
 int main(void)
